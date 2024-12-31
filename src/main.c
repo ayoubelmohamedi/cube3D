abce00a2e607b06b02907653808bc5478c008015
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-12-31 18:56:49 +0100

    Implement MinilibX initialization and window creation demo for testing minilibx

--- src/main.c
+++ src/main.c
@@ -6,8 +6,25 @@
 int main(int ac, char **av)
 {
 
+    void    *mlx;        
+    void    *win;
 
+    mlx = mlx_init();
+    if (!mlx)
+    {
+        write(2, "Error: Unable to initialize MinilibX\n", 37);
+        return (1);
+    }
 
+    win = mlx_new_window(mlx, 800, 600, "My Window");
+    if (!win)
+    {
+        write(2, "Error: Unable to create window\n", 31);
+        return (1);
+    }
+
+    // Wait for events (close window with any key press)
+    mlx_loop(mlx);
 
     return (0);
 }
