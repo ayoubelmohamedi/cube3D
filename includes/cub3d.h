7bc7287d2d4c1c0c29456ddc5b4c9a84dc1f091f
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-12-31 18:36:38 +0100

    Add necessary includes to cub3d.h

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,6 +3,12 @@
 #ifndef CUB3D_H
 #define CUB3D_H
 
+# include "mlx.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 
 #endif /* CUB3D_H */
