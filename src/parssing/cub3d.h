9b220bc33bed5de7a8a66468bc33464b002bd56d
ahmedez-zouine <zouineahmed976@gmail.com>
2025-01-16 19:04:35 +0100

    add some function in parsing

--- src/parssing/cub3d.h
+++ src/parssing/cub3d.h
@@ -8,6 +8,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "./../../get_next_line/get_next_line.h"
+# include <unistd.h>
+# include <string.h>
 
 /*
  * parsing textures and save data 
--- src/parssing/main_test.c
+++ src/parssing/main_test.c
@@ -4,6 +4,8 @@ int main(int argc, char *argv[])
 {
 	t_textures_data *texture_dt = NULL;
 
+	texture_dt = malloc(sizeof(t_textures_data));
+
 	if (argc != 2 || !argv[1])
 		ft_msg_exit("Error in argm\n");
 	check_file_map(argv[1], texture_dt);
