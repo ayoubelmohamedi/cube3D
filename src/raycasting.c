c697f3bb3807d26cb1e79ddfce6c45acb3199ea5
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-04-17 17:36:27 +0100

    Implement floor rendering logic and update ceiling texture handling

--- src/raycasting.c
+++ src/raycasting.c
@@ -114,12 +114,44 @@ int darken_color(int color, double dist)
 
 void render_floor(t_player *player, int screen_x, int y_start, int y_end, int height)
 {
-    (void)player;
-    (void)screen_x;
-    (void)y_start;
-    (void)y_end;
-    (void)height;
-    return;
+    (void) y_start;
+    (void) height;
+
+    double playerDirX = cos(player->dir);
+    double playerDirY = sin(player->dir);
+
+    double fov_rad = FOV * (M_PI / 180); 
+
+    double planeX = -playerDirY * tan(FOV / 2.0);
+    double planeY = playerDirX * tan(fov_rad / 2.0);
+
+    for (int y = y_end; y < HEIGHT; y++)
+    {
+        double rowDistance = (0.5 * HEIGHT) / (y - HEIGHT / 2.0);
+
+        float floorStepX = rowDistance * (playerDirX + planeX - (playerDirX - planeX)) / WIDTH;
+        float floorStepY = rowDistance * (playerDirY + planeY - (playerDirY - planeY)) / WIDTH;
+
+        float currentFloorX = player->x + rowDistance * (playerDirX - planeX);
+        float currentFloorY = player->y + rowDistance * (playerDirY - planeY);
+        
+        currentFloorX += floorStepX * screen_x;
+        currentFloorY += floorStepY * screen_x;
+
+        int texX = (int)(player->env->texture->floor_width * (currentFloorX - floor(currentFloorX))) & (player->env->texture->floor_width - 2);
+        int texY = (int)(player->env->texture->floor_height * (currentFloorY - floor(currentFloorY))) & (player->env->texture->floor_height - 1);
+
+        int floor_color = FLOOR_COLOR;
+        if (texX >= 0 && texX < player->env->texture->floor_width && texY >= 0 && texY < player->env->texture->floor_height)
+        {
+            char *tex_pixel_ptr = player->env->texture->floor_addr + (texY * player->env->texture->floor_line_len + texX * (player->env->texture->floor_bpp / 8));
+            floor_color = *(unsigned int*) tex_pixel_ptr;
+            floor_color = darken_color(floor_color, rowDistance);
+            my_mlx_pixel_put(player->env, screen_x, y, floor_color);
+        }
+    }
+
+
 } 
 
 void render_ceiling(t_player *player, int screen_x, int y_start, int y_end, int height)
@@ -156,7 +188,7 @@ void render_ceiling(t_player *player, int screen_x, int y_start, int y_end, int 
         int texX = (int)(player->env->texture->ceil_width * (currentCeilX - floor(currentCeilX))) & (player->env->texture->ceil_width - 1);
         int texY = (int)(player->env->texture->ceil_height * (currentCeilY - floor(currentCeilY))) & (player->env->texture->ceil_height - 1);
 
-        int ceil_color = 0;
+        int ceil_color = CEILING_COLOR;
         if (texX >= 0 && texX < player->env->texture->ceil_width && texY >= 0 && texY < player->env->texture->ceil_height)
         {
             char *tex_pixel_ptr = player->env->texture->ceil_addr + (texY * player->env->texture->ceil_line_len + texX * (player->env->texture->ceil_bpp / 8));
@@ -396,9 +428,17 @@ int main()
                    "assets/sky/zenith.xpm",
                    NULL};
 
+    // concrete & grass & planks
+    char *floor[] = { "assets/floor/concrete/lime.xpm",
+        "assets/floor/concrete/yellow.xpm",
+        "assets/floor/grass/AC.xpm",
+        "assets/floor/planks/birch.xpm",
+        NULL
+    };
+
     // adaptive part
     texture.has_ceiling = true;
-    texture.has_floor = false;
+    texture.has_floor = true;
     env.has_texture = true;
     env.texture = &texture;
 
@@ -421,6 +461,20 @@ int main()
         perror("celiling image error");
         return (1);
     }
+    texture.floor_img = mlx_xpm_file_to_image(env.mlx, floor[1], &env.texture->floor_width, &env.texture->floor_height);
+    if (!env.texture->ceil_img)
+    {
+        // exit
+        // ft_destory();
+        //  print_error();
+        mlx_destroy_image(env.mlx ,texture.ceil_img);
+        mlx_destroy_image(env.mlx, env.img);
+        mlx_destroy_window(env.mlx, env.win);
+        perror("floor image error");
+        return (1);
+    }
+
+    env.texture->floor_addr = mlx_get_data_addr(env.texture->floor_img, &env.texture->floor_bpp, &env.texture->floor_line_len, &env.texture->floor_endian);
     env.texture->ceil_addr = mlx_get_data_addr(env.texture->ceil_img, &env.texture->ceil_bpp, &env.texture->ceil_line_len, &env.texture->ceil_endian);
 
     double player_x = 3.5;
