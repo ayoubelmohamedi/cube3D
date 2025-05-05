318454c10d99835367ad108fae4bca2b565c7bd2
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-05-05 21:57:38 +0100

    fix: add rgb field to map structure and include parsing header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 08:56:09 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/05/05 21:42:19 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/05/05 21:56:34 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #define CUB3D_H
 
 #include "game_macros.h"
+#include "parsing.h"
 #include "mlx.h"
 #include <math.h>
 #include <stdlib.h>
@@ -90,6 +91,7 @@ typedef struct
 typedef struct
 {
     char **map;
+    t_rgb *rgb;
     int map_width;
     int map_height;
     char *textures_files[2];
