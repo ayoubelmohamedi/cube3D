016a96f0d36559b511b9fe160656209d8b782410
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-05-10 20:23:35 +0100

    fix norm: effects.c, used t_rgb_info

--- src/efffects.c
+++ src/efffects.c
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 08:56:00 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/05/10 18:25:42 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/05/10 20:22:50 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@ int	get_color(int wall_type)
 	return (0xAAAAAA);
 }
 
-// Darken the color based on distance
+// Darken the color based on distance fade with distance
 int	darken_color(int color, double dist)
 {
 	t_rgb_info	rgb;
+	double		factor;
 
-	double factor = 1.0 / (1.0 + dist * 0.05); // fade with distance
+	factor = 1.0 / (1.0 + dist * 0.05);
 	rgb.r = ((color >> 16) & 0xFF) * factor;
 	rgb.g = ((color >> 8) & 0xFF) * factor;
 	rgb.b = (color & 0xFF) * factor;
@@ -63,20 +64,18 @@ int	ceil_fog_color(int ceil_color, int rowDistance)
 
 int	vignette_effect(int curr_x, int color)
 {
-	double	vignette_strength;
-	double	dist_from_center;
-	double	vignette_factor;
-	int		r;
-	int		g;
-	int		b;
+	double		vignette_strength;
+	double		dist_from_center;
+	double		vignette_factor;
+	t_rgb_info	rgb;
 
 	vignette_strength = 0.4;
 	dist_from_center = fabs((double)curr_x - WIDTH / 2.0) / (WIDTH / 2.0);
 	vignette_factor = 1.0 - dist_from_center * vignette_strength;
 	if (vignette_factor < 0.0)
 		vignette_factor = 0.0;
-	r = ((color >> 16) & 0xFF) * vignette_factor;
-	g = ((color >> 8) & 0xFF) * vignette_factor;
-	b = (color & 0xFF) * vignette_factor;
-	return ((r << 16) | (g << 8) | b);
+	rgb.r = ((color >> 16) & 0xFF) * vignette_factor;
+	rgb.g = ((color >> 8) & 0xFF) * vignette_factor;
+	rgb.b = (color & 0xFF) * vignette_factor;
+	return ((rgb.r << 16) | (rgb.g << 8) | rgb.b);
 }
